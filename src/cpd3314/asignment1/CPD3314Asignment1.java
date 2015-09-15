8335c271c67585f27affb49144f70fb8d3b43f8c
Harpreet Kaur <c0663712@mylambton.ca>
2015-09-15 15:41:46 -0400

    find the potential salary

--- src/cpd3314/asignment1/CPD3314Asignment1.java
+++ src/cpd3314/asignment1/CPD3314Asignment1.java
@@ -17,6 +17,44 @@ public class CPD3314Asignment1 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello World");
+        /*
+        * Assigment#1
+        
+        Job posting
+        TekHunter
+        Senior Java Developer 
+        Greater Toronto Area, ON
+        Experience : 3-8 YEARS
+        Skills-JavaScript, jQuery, JSP and AJAX
+        http://careers.wipro.com/share-search-for-jobs.aspx?jobcode=398290&Subsource=INDED&Source=WEB&utm_source=Indeed&utm_medium=organic&utm_campaign=Indeed
+        
+        Mandatory Skills:
+        Core Java, Java-J2EE
+        
+       javascript- JavaScript is the programming language of HTML and the Web.
+      Programming makes computers do what you want them to do.
+        https://www.javascript.com/
+        
+        
+       jQuery- jQuery is a fast, small, and feature-rich JavaScript library.
+        https://jquery.com/
+        
+        Java-J2EE -Java 2 Platform, Enterprise Edition
+        http://searchsoa.techtarget.com/definition/J2EE
+        
+         
+        
+        
+        
+        Salary
+        C$69,278 -toronto On
+        C$71,080 -Waterloo-ON
+        C$69,278- Mississauga
+        C$66,742-Vancouver, BC
+        
+        
+        
+        */
         
     }
     
